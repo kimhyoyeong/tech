c14ae5383439a826ab1a2add78dcfaa57db8a202
[웹퍼블리셔]김효영 <kimhyo089@naver.com>
2018-07-31 16:21:07 +0900

    필요한것만.

--- 06/test/src/index.js
+++ 06/test/src/index.js
@@ -1,5 +1,5 @@
-import entry from './js/entry';
 import style from './scss/style.scss';
+import style2 from './scss/style2.scss';
 
 
 import index from '../index.html';//추가해야지만 HMR 가능
--- 06/test/webpack.config.js
+++ 06/test/webpack.config.js
@@ -1,12 +1,11 @@
 const webpack = require('webpack');
 const path = require('path');
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
-	mode: 'development',
-	//production 배포용일 경우 알아서 최적화가 됨
+	mode : 'development',
 
 	entry: {
 		index: './src/index.js'
@@ -14,34 +13,19 @@ module.exports = {
 
 	output: {
 		path: path.resolve(__dirname, 'dist'),
-		//path.resolve([from…], to)전달받은 경로의 절대 경로를 리턴합니다'C:\\from\\to'
-		//__dirname 은 항상 현재 파일의 디렉토리
-
-		//publicPath: '/dist',
-		// 클라이언트가 빌드된 파일에 접근할 수 있도록 서버가 제공할 path
-		// CDN 주소 사용 가능
-
-		filename: '[name].bundle.js'
-		// 결과물 파일명
-		// app.js -> app.js
-		// [name].js -> entry에서 설정한 이름(object key).js
-		// [hash].js -> 빌드마다 변경되는 해시.js
-		// [chunkhash].js -> 파일 고유의 해시(파일이 달라질 경우 변경됨).js
+		filename: 'bundle.js'
 	},
 
 	module: {
 		rules: [
-			// {//bundle.js로 합쳐짐
-			//     test:/\.(s*)css$/,
-			//     use: ['style-loader','css-loader','sass-loader']
-			// },
-
-			{//css로 추출 플러그인 사용
-				test: /\.(s*)css$/,
-				use: ExtractTextPlugin.extract({
-					fallback: 'style-loader',
-					use: ['css-loader?sourceMap', 'sass-loader?sourceMap'],
-				})
+			{
+				test: /\.s?css$/,
+				use: [
+					'css-hot-loader',
+					MiniCssExtractPlugin.loader,
+					{ loader: 'css-loader', options: { url: false, sourceMap: true } },
+					{ loader: 'sass-loader', options: { sourceMap: true } }
+				]
 			},
 
 			{
@@ -51,7 +35,6 @@ module.exports = {
 					loader: "babel-loader"
 				}
 			},
-
 			{
 				test: /\.(ico|png|jpg|jpeg|gif|svg|woff|woff2|ttf|eot)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
 				loader: 'url-loader',
@@ -60,7 +43,6 @@ module.exports = {
 					limit: 10000
 				}
 			},
-
 			{
 				test: /\.html$/,
 				loader: "raw-loader"
@@ -69,7 +51,6 @@ module.exports = {
 	},
 
 	plugins: [
-		//build전에 결과물이 생성되는 파일을 비워줌
 		new CleanWebpackPlugin(['dist']),
 
 		//jquery
@@ -78,26 +59,12 @@ module.exports = {
 			jQuery: 'jquery'
 		}),
 
-		// HMR을 전체적으로 사용할수 있도록 설정
-		//new webpack.HotModuleReplacementPlugin(),
-		//package.json  --hot 추가해도 작동함
-
-		//css추출 플러그인
-		new ExtractTextPlugin({
-			//filename: "styles.css",
-
-			filename : '[name].css',
-			// // entry에 선언된 객체의 각 프로퍼티가 [name]과 치환되어 파일이 생성
-			// disable : false,
-			allChunks : true
+		//cssExtract
+		new MiniCssExtractPlugin({
+			filename: 'bundle.css'
 		}),
 
-		//공통 모듈로 구성된 파일
-		// new webpack.optimize.CommonsChunkPlugin({
-		// 	name: 'common'
-		// }),
-
-		//html build
+		//htmlBuild
 		new HtmlWebpackPlugin({
 			title: 'index',
 			hash: true,
@@ -107,7 +74,6 @@ module.exports = {
 		})
 
 	],
-
 
 	//최적화
 	optimization: {
@@ -121,18 +87,9 @@ module.exports = {
 
 	devServer: {
 		contentBase: path.join(__dirname, 'dist'),
-		//output path를 match하도록 합니다.
-		//path.join(path1, path2…) 파라미터로 전달받은 경로를 이어서 하나의 경로로 만듭니다 'path1\\path2'
-
 		hot: true
-		//server에 HMR을 사용할수 있도록 설정
-
-		//publicPath: '/dist'
-		//publicPath의 ouput 같게 설정
 	},
-
-	devtool: 'eval-source-map',
-
+	devtool: 'source-map',
 	performance: {
 		hints: process.env.NODE_ENV === 'production' ? "warning" : false
 	}
